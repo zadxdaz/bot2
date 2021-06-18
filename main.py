419b0023b667745d325a8541c59be42e937ef0eb
zadxdaz <82950029+zadxdaz@users.noreply.github.com>
2021-06-18 16:20:08 -0300

    bugfixes

--- main.py
+++ main.py
@@ -17,13 +17,17 @@ def buscar_tren(imagen):
         return pos
     except:
         print("error al buscar el tren")
+        return False
+def click_on_thing(imagen):
+    if buscar_tren(imagen) != False:
+        pg.click(buscar_tren(imagen))
 
 
 def menu():
     layout = [[sg.Button("Setear flecha", key="set_flecha")],
               [sg.Button("Empezar", key="start")],
-              [sg.Button("Stop", key="stop")],
-              [sg.Button("Buscar Tren",key="tren")],
+              [sg.Button("Stop", key="stop"),sg.Input(" ",key="IN"),sg.FileBrowse("Buscar imagen del tren",key="tren_imag",target="IN")],
+              [sg.Button("Probar Tren",key="tren"),sg.Button("Start auto tren",key="start_tren")],
               [sg.Text("Color         ", key="color")]]
     window = sg.Window("Menu", layout)
     return window
@@ -34,10 +38,12 @@ def main():
     flecha_y = 0
     menu_window = menu()
     proceso = False
-    tiempo = 0
-    tren_imagen_link="tren.png"
+    process_timer = 0
+    tren_imagen_link="tren_notebook.png"
+    tren_timer=0
+    tren_process=False
     while True:
-        event, values = menu_window.read(timeout=34)
+        event, values = menu_window.read(timeout=100)
         if event == sg.WIN_CLOSED:
             break
         if event == "set_flecha":
@@ -48,23 +54,38 @@ def main():
         if event == "stop":
             proceso = False
         if proceso == True :
-            if tiempo == 0:
-                tiempo = time.time()
-            if time.time() >= tiempo:
+            if process_timer == 0:
+                process_timer = time.time()
+            if time.time() >= process_timer:
                 color = pg.pixel(flecha_x, flecha_y)
                 menu_window["color"].update(color)
                 if pg.pixelMatchesColor(flecha_x, flecha_y, (255, 255, 255), tolerance=10):
                     print("white")
-                tiempo = tiempo + 5.0
+                process_timer = process_timer + 5.0
 
         if event == "start":
             if flecha_x == 0 or flecha_y == 0:
                 print("No seteaste la flecha pelotudo")
                 continue
             proceso = True
+        if event == "tren_imag":
+            print(values["IN"])
+            print("a")
 
         if event == "tren":
-            pg.click(buscar_tren(tren_imagen_link))
+            tren_imagen_link=values["IN"]
+            click_on_thing(tren_imagen_link)
+
+        if event== "start_tren":
+            tren_process=True
+
+        if tren_process:
+            if tren_timer == 0:
+                tren_timer = time.time()
+                continue
+            if time.time() >= tren_timer:
+                click_on_thing(tren_imagen_link)
+                tren_timer += 300
 
 
 if __name__ == '__main__':
